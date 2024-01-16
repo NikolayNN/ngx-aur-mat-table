f7b7242b526089392ca69a0fcae86c688e7b3288
Nikolay <nikolay.horushko@gmail.com>
2024-01-16 14:53:11 +0300

    version: 12.2.18 implement util for table-config to get keyNameMap

--- projects/ngx-aur-mat-table/src/lib/utils/ngx-aur-table-config.util.ts
+++ projects/ngx-aur-mat-table/src/lib/utils/ngx-aur-table-config.util.ts
@@ -8,4 +8,11 @@ export class NgxAurTableConfigUtil {
   public static namedKeys(config: TableConfig<any>): {name: string, key: string}[] {
     return config.columnsCfg.map(cfg => ({name: cfg.name, key: cfg.key}));
   }
+
+  /**
+   * @return Map where the key is 'key' and the value is 'name'
+   */
+  public static keyNameMap(config: TableConfig<any>): Map<string, string> {
+    return new Map(config.columnsCfg.map(cfg => [cfg.key, cfg.name]));
+  }
 }
