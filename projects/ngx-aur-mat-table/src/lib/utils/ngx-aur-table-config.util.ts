12c04e32cc9d9771bcd8e617798c5b1e292cc1f5
Nikolay <nikolay.horushko@gmail.com>
2024-01-16 16:39:04 +0300

    version: 12.2.21

--- projects/ngx-aur-mat-table/src/lib/utils/ngx-aur-table-config.util.ts
+++ projects/ngx-aur-mat-table/src/lib/utils/ngx-aur-table-config.util.ts
@@ -4,20 +4,19 @@ import {IndexProvider} from "../providers/IndexProvider";
 import {RowActionProvider} from "../providers/RowActionProvider";
 
 export class NgxAurTableConfigUtil {
-  private static readonly GENERATED_KEYS = new Set([SelectionProvider.COLUMN_NAME, IndexProvider.COLUMN_NAME, RowActionProvider.COLUMN_NAME]);
 
   public static keys(config: TableConfig<any>): string[] {
-    return this.managingColumns(config).map(c => c.key);
+    return this.columnCfgs(config).map(c => c.key);
   }
 
-  private static managingColumns(config: TableConfig<any>): ColumnConfig<any>[] {
-    return config.columnsCfg.filter(cfg => !this.GENERATED_KEYS.has(cfg.key))
+  private static columnCfgs(config: TableConfig<any>): ColumnConfig<any>[] {
+    return config.columnsCfg;
   }
 
   /**
    * @return Map where the key is 'key' and the value is 'name'
    */
   public static keyNameMap(config: TableConfig<any>): Map<string, string> {
-    return new Map(this.managingColumns(config).map(cfg => [cfg.key, cfg.name]));
+    return new Map(this.columnCfgs(config).map(cfg => [cfg.key, cfg.name]));
   }
 }
