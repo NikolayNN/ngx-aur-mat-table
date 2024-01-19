63b45272e86f953207b65783eca1ae563e4d079e
Nikolay <nikolay.horushko@gmail.com>
2024-01-19 18:36:18 +0300

    version: 12.2.28: fix

--- projects/ngx-aur-mat-table/src/lib/ngx-aur-mat-table.component.ts
+++ projects/ngx-aur-mat-table/src/lib/ngx-aur-mat-table.component.ts
@@ -53,8 +53,10 @@ export class NgxAurMatTableComponent<T> implements OnInit, OnChanges, AfterViewI
   _customDisplayColumnsEnabled = false;
 
   @Input() set displayColumns(columns: string[]) {
-    this._displayColumns = [...columns];
-    this._customDisplayColumnsEnabled = columns && columns.length > 0;
+    if (columns) {
+      this._displayColumns = [...columns];
+      this._customDisplayColumnsEnabled = columns && columns.length > 0;
+    }
   }
 
   tableView: Map<string, ColumnView<string>>[] = [];
